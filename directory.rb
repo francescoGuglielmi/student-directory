f0046584143cb6e0202ef14d88c78150c9d082e2
FrancescoGuglielmi <francesco.guglielmi.work@outlook.com>
2023-02-08 11:49:27 +0000

    centered name

--- directory.rb
+++ directory.rb
@@ -1,14 +1,25 @@
 
 
 def input_students
-  puts "Please enter the name of the students"
-  puts "To finish, just hit return twice"
   students = []
+  puts "Please enter the name of the students."
+  puts "To finish, just hit return "
   name = gets.chomp
   while !name.empty? do
-    students << {name: name.capitalize!, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
+    puts "Please enter a hobby."
+  puts "To finish, just hit return "
+  hobby = gets.chomp
+  puts "Please enter a gender identity."
+  puts "To finish, just hit return "
+  gender = gets.chomp
+  puts "Please enter a nationality."
+  puts "To finish, just hit return "
+  nationality = gets.chomp
+  students << {name: name, cohort: :november, hobby: hobby, gender: gender, nationality: nationality }
+  puts "Now we have #{students.count} students"
+  puts "Please enter the name of the students."
+  puts "To finish, just hit return "
+  name = gets.chomp
   end 
   students
 end 
@@ -20,18 +31,16 @@ end
 
 def print(students) 
   students.each_with_index {|student, index| 
-  if student[:name][0] == "T" 
-    if student[:name].length < 12
+    if student[:name][0].upcase == "T" && student[:name].length < 12 
       n = 0
       count = 1
       while n <= count do
-        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" 
+        puts "#{index + 1}. #{student[:name].center(student[:name].length + 6)} (#{student[:cohort]} cohort)" 
         n += 1
         count = 0
       end
     end
-  end
-}
+  }
 end
 
 def print_footer(students)
