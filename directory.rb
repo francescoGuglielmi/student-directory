7a48f6abf507ee5686f464acdf078a6343935f14
FrancescoGuglielmi <francesco.guglielmi.work@outlook.com>
2023-02-10 12:51:58 +0000

    added save_students method with target file

--- directory.rb
+++ directory.rb
@@ -64,6 +64,7 @@ end
 def print_menu 
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -79,6 +80,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -94,5 +97,13 @@ def interactive_menu
   end
 end
 
-
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 interactive_menu
