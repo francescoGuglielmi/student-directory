27a03f5cf0a0335ed07170ce28e383e7de5f97b7
FrancescoGuglielmi <francesco.guglielmi.work@outlook.com>
2023-02-10 15:42:37 +0000

    improved readability by adding context to the code

--- directory.rb
+++ directory.rb
@@ -1,36 +1,5 @@
-@students = []
-  
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit enter twice"
-  name = STDIN.gets.chomp
-  puts "Please enter a hobby"
-  hobby = STDIN.gets.chomp
-  puts "Please enter a nationality"
-  nationality = STDIN.gets.chomp
-  while !name.empty? do
-    @students << {name: name, cohort: :november, hobby: hobby}
-    puts "Now we have #{@students.count} students"
-    name = gets.chomp
-  end
-end
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
-end 
-
-def print_students_list
-  i = 0
-  while i < @students.length do
-    puts "#{@students[i][:name]}, hobby: #{@students[i][:hobby]}, nationality: #{@students[i][:nationality]} (#{@students[i][:cohort]} cohort)"
-    i += 1
-  end
-end
+# ALTERNATIVE METHODS TO print_student_list
 
 def print_with_index
   @students.each_with_index do |student, index|
@@ -60,12 +29,38 @@ def print_centered
   end
 end  
 
+# RAW ESSENTIAL
+
+@students = []
+
+def interactive_menu
+  loop do 
+    print_menu 
+    process(STDIN.gets.chomp)
+  end
+end
+
 def print_menu 
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
   puts "9. Exit"
+end
+  
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit enter twice"
+  name = STDIN.gets.chomp
+  puts "Please enter a hobby"
+  hobby = STDIN.gets.chomp
+  puts "Please enter a nationality"
+  nationality = STDIN.gets.chomp
+  while !name.empty? do
+    @students << {name: name, cohort: :november, hobby: hobby}
+    puts "Now we have #{@students.count} students"
+    name = gets.chomp
+  end
 end
 
 def show_students
@@ -74,29 +69,22 @@ def show_students
   print_footer
 end
 
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
-  else
-    puts "I don't know what you meant, try again."
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_students_list
+  i = 0
+  while i < @students.length do
+    puts "#{@students[i][:name]}, hobby: #{@students[i][:hobby]}, nationality: #{@students[i][:nationality]} (#{@students[i][:cohort]} cohort)"
+    i += 1
   end
 end
 
-def interactive_menu
-  loop do 
-    print_menu 
-    process(STDIN.gets.chomp)
-  end
-end
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end 
 
 def save_students
   file = File.open("students.csv", "w")
@@ -117,6 +105,23 @@ def load_students(filename = "students.csv")
   file.close
 end
 
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again."
+  end
+end
+
 def try_load_students
   filename = ARGV.first
   return if filename.nil?
@@ -128,6 +133,8 @@ def try_load_students
     exit
   end
 end
+
+# METHODS CALLED:
 
 try_load_students
 interactive_menu
