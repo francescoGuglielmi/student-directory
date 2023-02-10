d98ab7472ef1bd6af70b94f342aa7c7ddfa2d8c0
FrancescoGuglielmi <francesco.guglielmi.work@outlook.com>
2023-02-10 12:07:13 +0000

    refactored some methods to improve readability

--- directory.rb
+++ directory.rb
@@ -1,33 +1,19 @@
-
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november, hobby: "scare"},
-  {name: "Darth Vader", cohort: :november, hobby: "killing"},
-  {name: "Nurse Ratched", cohort: :november, hobby: "scare"},
-  {name: "Michael Corleone", cohort: :november, hobby: "killing"},
-  {name: "Alex DeLarge", cohort: :november, hobby: "unknown"},
-  {name: "The Wicked Witch of the West", cohort: :november, hobby: "unknown"},
-  {name: "Terminator", cohort: :november, hobby: "killing"},
-  {name: "Freddy Krueger", cohort: :november, hobby: "scare"},
-  {name: "The Joker", cohort: :november, hobby: "scare"},
-  {name: "Joffrey Baratheon", cohort: :november, hobby: "killing"},
-  {name: "Norman Bates", cohort: :november, hobby: "unknown"}
-  ]
+@students = []
   
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit enter twice"
-  students = []
+  @students
   name = gets.chomp
   puts "Please enter a hobby"
   hobby = gets.chomp
   puts "Please enter a nationality"
   nationality = gets.chomp
-  while !name.empty? {
-    students << {name: name, cohort: :november, hobby: hobby}
-    puts "Now we have #{students.count} students"
+  while !name.empty? do
+    @students << {name: name, cohort: :november, hobby: hobby}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
-  }
-  students
+  end
 end
 
 def print_header
@@ -35,47 +21,78 @@ def print_header
   puts "-------------"
 end
 
-def print_footer(array)
-  puts "Overall, we have #{array.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end 
 
-def print(array)
+def print_students_list
   i = 0
   while i < array.length do
-    puts "#{array[i][:name]}, hobby: #{array[i][:hobby]}, (#{array[i][:cohort]} cohort)"
+    puts "#{@students[i][:name]}, hobby: #{@students[i][:hobby]}, (#{@students[i][:cohort]} cohort)"
     i += 1
   end
 end
 
-def print_with_index(array)
-  array.each_with_index do |student, index|
+def print_with_index
+  @students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_T_villains(array)
-  array.each do |student|
+def print_T_villains
+  @students.each do |student|
     if student[:name][0].upcase == "T"
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
 end
 
-def print_shortName_villains(array)
-  array.each do |student|
+def print_shortName_villains
+  @students.each do |student|
     if student[:name].length < 12
       puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
 end
 
-def print_centered(array)
-  array.each do |student| 
+def print_centered
+  @students.each do |student| 
     puts "#{student[:name].center(student[:name].length + 8)} hobby: #{student[:hobby]} (#{student[:cohort]} cohort)"
   end
 end  
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def print_menu 
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again."
+  end
+end
+
+def interactive_menu
+  @students
+  loop do 
+    print_menu 
+    process(gets.chomp)
+  end
+end
+
+
+interactive_menu
