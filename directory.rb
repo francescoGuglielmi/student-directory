5519b7344f3934825232a0b34048e5632e89f209
FrancescoGuglielmi <francesco.guglielmi.work@outlook.com>
2023-02-10 10:47:10 +0000

    rewrote program in a more readable way by separating tasks in defined methods

--- directory.rb
+++ directory.rb
@@ -1,58 +1,79 @@
 
-
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november, hobby: "scare"},
+  {name: "Darth Vader", cohort: :november, hobby: "killing"},
+  {name: "Nurse Ratched", cohort: :november, hobby: "scare"},
+  {name: "Michael Corleone", cohort: :november, hobby: "killing"},
+  {name: "Alex DeLarge", cohort: :november, hobby: "unknown"},
+  {name: "The Wicked Witch of the West", cohort: :november, hobby: "unknown"},
+  {name: "Terminator", cohort: :november, hobby: "killing"},
+  {name: "Freddy Krueger", cohort: :november, hobby: "scare"},
+  {name: "The Joker", cohort: :november, hobby: "scare"},
+  {name: "Joffrey Baratheon", cohort: :november, hobby: "killing"},
+  {name: "Norman Bates", cohort: :november, hobby: "unknown"}
+  ]
+  
 def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit enter twice"
   students = []
-  cohorts = []   # I initialize cohort
-  puts "Please enter the name of the students."
-  puts "To finish, just hit return "
   name = gets.chomp
-  while !name.empty? do
-    puts "what cohort is the student in? "
-    cohort = gets.chomp
-    if cohort == ""
-      cohort = "november"
-    end
-    cohorts << cohort     # I shovel every cohort inside array cohorts
-    puts "Please enter a hobby."
-    hobby = gets.chomp
-    puts "Please enter a gender identity."
-    gender = gets.chomp
-    puts "Please enter a nationality."
-    nationality = gets.chomp
-    students << {name: name, cohort: cohort.to_sym, hobby: hobby, gender: gender, nationality: nationality }
+  puts "Please enter a hobby"
+  hobby = gets.chomp
+  puts "Please enter a nationality"
+  nationality = gets.chomp
+  while !name.empty? {
+    students << {name: name, cohort: :november, hobby: hobby}
     puts "Now we have #{students.count} students"
-    puts "Please enter the name of the students."
-    puts "To finish, just hit return "
     name = gets.chomp
-  end 
+  }
   students
-   # Here I was supposed to "return cohorts" but if I do the console throws me an error 
-   # "no implicit conversion from Symbol to Integer" I don't know how to fix this.
-   # I was wishing to use that array to check students[:cohort] against it to print the names grouped in cohorts.
-end 
+end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students) 
-  students.each_with_index {|student, index| 
-    if student[:name][0].upcase == "T" && student[:name].length < 12 
-      n = 0
-      count = 1
-      while n <= count do
-        puts "#{index + 1}. #{student[:name].center (student[:name].length + 6)} (#{student[:cohort]} cohort)" 
-        n += 1
-        count = 0
-      end
-    end   
-  }
+def print_footer(array)
+  puts "Overall, we have #{array.count} great students"
+end 
+
+def print(array)
+  i = 0
+  while i < array.length do
+    puts "#{array[i][:name]}, hobby: #{array[i][:hobby]}, (#{array[i][:cohort]} cohort)"
+    i += 1
+  end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
-end 
+def print_with_index(array)
+  array.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_T_villains(array)
+  array.each do |student|
+    if student[:name][0].upcase == "T"
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_shortName_villains(array)
+  array.each do |student|
+    if student[:name].length < 12
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_centered(array)
+  array.each do |student| 
+    puts "#{student[:name].center(student[:name].length + 8)} hobby: #{student[:hobby]} (#{student[:cohort]} cohort)"
+  end
+end  
 
 students = input_students
 print_header
